9ad48aca59e4d1cfb8a67b6414c3536aafcd7c0f
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-11-22 22:30:08 +0900

    ✨Feat : 대시보드 상품 추가 UI 개발

--- project/src/components/admin/Dashboard.tsx
+++ project/src/components/admin/Dashboard.tsx
@@ -1,8 +1,223 @@
+"use client";
+
+import { postProdct } from "@/services/productService";
+import { Product } from "@/types/models/product.type";
+import React, { useEffect, useState } from "react";
+
 const Dashboard = () => {
+    const [formData, setFormData] = useState<Product>({
+        name: "",
+        link: "",
+        src: "",
+        major_category: "제로칼로리",
+        middle_category: "음료",
+        price: 0,
+        piece: 0,
+        price_per_piece: 0,
+        nutritional: {
+            calories: 0,
+            carbohydrate: 0,
+            protein: 0,
+            fat: 0,
+        },
+    });
+
+    // price_per_piece 계산
+    useEffect(() => {
+        setFormData((prevData) => ({
+            ...prevData,
+            price_per_piece:
+                prevData.piece !== 0 ? parseFloat((prevData.price / prevData.piece).toFixed(0)) : 0,
+        }));
+    }, [formData.price, formData.piece]);
+
+    // input 데이터 변경 감지 핸들러
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+
+        // nutritional 객체 내부 입력값 감지
+        if (["calories", "carbohydrate", "protein", "fat"].includes(name)) {
+            setFormData((prevData) => ({
+                ...prevData,
+                nutritional: {
+                    ...prevData.nutritional,
+                    [name]: Number(value), // 숫자 변환
+                },
+            }));
+        }
+        // 일반적인 입력값 감지
+        else {
+            setFormData({
+                ...formData,
+                [name]: name === "price" || name === "piece" ? Number(value) : value, // 숫자 필드 변환
+            });
+        }
+    };
+
+    // form 제출 핸들러
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const reponse = await postProdct([formData]);
+
+        if (reponse) {
+            const { data, error } = reponse;
+            console.log("success POST : ", data);
+            console.log("에러 내용 : ", error);
+            ClearFormData();
+        }
+
+    };
+
+
+    // 폼 데이터 초기화
+    const ClearFormData = () => {
+        setFormData({
+            name: "",
+            link: "",
+            src: "",
+            major_category: "제로칼로리",
+            middle_category: "음료",
+            price: 0,
+            piece: 0,
+            price_per_piece: 0,
+            nutritional: {
+                calories: 0,
+                carbohydrate: 0,
+                protein: 0,
+                fat: 0,
+            },
+        });
+    }
+
+    // 입력값 콘솔
+    // useEffect(() => {
+    //     console.log(formData);
+    // }, [formData]);
+
     return (
-        <div>
-            대시보드 컴포넌트
-        </div>
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label>name:</label>
+                <input type="text" name="name" value={formData.name} onChange={handleChange} />
+            </div>
+
+            <div>
+                <label>link:</label>
+                <input type="text" name="link" value={formData.link} onChange={handleChange} />
+            </div>
+
+            <div>
+                <label>src:</label>
+                <input type="text" name="src" value={formData.src} onChange={handleChange} />
+            </div>
+
+            <div>
+                <label>major_category:</label>
+                <input
+                    type="text"
+                    name="major_category"
+                    value={formData.major_category}
+                    disabled
+                />
+            </div>
+
+            <div>
+                <label>middle_category:</label>
+                <input
+                    type="text"
+                    name="middle_category"
+                    value={formData.middle_category}
+                    disabled
+                />
+            </div>
+
+            <div>
+                <label>price:</label>
+                <input
+                    type="number"
+                    name="price"
+                    value={formData.price}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <label>piece:</label>
+                <input
+                    type="number"
+                    name="piece"
+                    value={formData.piece}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <label>price_per_piece:</label>
+                <input
+                    type="number"
+                    name="price_per_piece"
+                    value={formData.price_per_piece}
+                    disabled
+                />
+            </div>
+
+            {/* Nutritional Data */}
+            <div>
+                <label>calories:</label>
+                <input
+                    type="number"
+                    name="calories"
+                    value={formData.nutritional.calories}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <label>carbohydrate:</label>
+                <input
+                    type="number"
+                    name="carbohydrate"
+                    value={formData.nutritional.carbohydrate}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <label>protein:</label>
+                <input
+                    type="number"
+                    name="protein"
+                    value={formData.nutritional.protein}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <div>
+                <label>fat:</label>
+                <input
+                    type="number"
+                    name="fat"
+                    value={formData.nutritional.fat}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <button
+                style={{
+                    backgroundColor: "blue",
+                    color: "white",
+                    fontSize: "20px",
+                    width: "200px",
+                    height: "50px",
+                    border: "0px",
+                    borderRadius: "10px",
+                    margin: "40px 0px",
+                }}
+                type="submit"
+            >
+                Submit
+            </button>
+        </form>
     );
 };
 
