b4ff2e57f0475c462337511ca46ed8cd652f823b
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-11-30 18:26:58 +0900

    ✨ Feat : 회원가입 & 로그인 기능 개발

--- project/src/components/home/ItemContainer.tsx
+++ project/src/components/home/ItemContainer.tsx
@@ -3,26 +3,44 @@
 import useProduct from "@/hooks/useProduct";
 import { useEffect } from "react";
 import Item from "./Item";
-
+import { supabase } from "@/lib/supabaseClient";
 
 const ItemContainer = () => {
-    const { getAllProducts, responseData, error } = useProduct();
+  const { getAllProducts, responseData, error } = useProduct();
 
-    useEffect(() => {
-        getAllProducts();
-    }, []);
+  async function signUpNewUser() {
+    const { data, error } = await supabase.auth.signUp({
+      email: "m3088787@naver.com",
+      password: "au95ju04!!",
+    });
+    console.log("data : ", data);
+  }
 
-    return (
-        <div>
-            {
-                responseData?.map((el, idx) => {
-                    return (
-                        <Item key={idx} data={el} />
-                    )
-                })
-            }
-        </div>
-    );
+  async function signInWithEmail() {
+    const { data, error } = await supabase.auth.signInWithPassword({
+      email: "m3088787@naver.com",
+      password: "au95ju04!!",
+    });
+
+    console.log("data : ", data);
+  }
+
+  useEffect(() => {
+    getAllProducts();
+    signInWithEmail();
+    // const { data, error } = await supabase.auth.signUp({
+    //     email: 'm3088787@naver.com',
+    //     password: 'au95ju04!!',
+    //   })
+  }, []);
+
+  return (
+    <div style={{ display: "flex", flexWrap: "wrap", justifyContent: "center" }}>
+      {responseData?.map((el, idx) => {
+        return <Item key={idx} data={el} />;
+      })}
+    </div>
+  );
 };
 
 export default ItemContainer;
