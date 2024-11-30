8da8503801ca62a06e60c851239c1b134f42d0d7
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-11-30 13:29:56 +0900

    ✨ Feat : 아이템 스크롤 스냅 기능 추가

--- project/src/components/home/Item.tsx
+++ project/src/components/home/Item.tsx
@@ -6,6 +6,8 @@ const Item = ({ data }: { data: ProductWithId }) => {
   const { calories, carbohydrate, protein, fat } = nutritional;
   return (
     <Container href={link} target="_blank" rel="noopener noreferrer">
+      <div style={{display:"flex",}}>
+
       <Top>
         <Img src={src} alt="" />
         <div>상품명 : {name}</div>
@@ -23,6 +25,7 @@ const Item = ({ data }: { data: ProductWithId }) => {
         <div>단백질 : {protein}</div>
         <div>지방 : {fat}</div>
       </Bottom>
+      </div>
     </Container>
   );
 };
@@ -31,10 +34,10 @@ export default Item;
 
 const Container = styled.a`
   display: flex;
-  flex-direction: column;
-  flex: 1;
-  width: 200px;
+  width:200px;
   height: 400px;
+  overflow-x:scroll;
+  scroll-snap-type: x mandatory;
   border: 3px solid #26539c;
   text-decoration: none;
   color: inherit;
@@ -42,16 +45,17 @@ const Container = styled.a`
 `;
 
 const Img = styled.img`
-  width: 100%; /* 원하는 너비 */
+  width: 200px; /* 원하는 너비 */
   height: auto;
 `;
 
 const Top = styled.div`
+  scroll-snap-align: center;
   background-color: lightblue;
-  flex: 1 1 50%; /* 기본 크기는 콘텐츠에 따라, 여유 공간 비율로 확장 */
+  width: 200px;
+  height: 400px;
 `;
 
-const Bottom = styled.div`
+const Bottom = styled(Top)`
   background-color: yellow;
-  flex: 1 1 50%; /* 기본 크기는 콘텐츠에 따라, 여유 공간 비율로 확장 */
 `;
