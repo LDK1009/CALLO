0f02a203406609d56859cac9563196e475deec6f
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-11-30 18:25:42 +0900

    💄 Style : 상품 디자인 초기 개발

--- project/src/components/home/Item.tsx
+++ project/src/components/home/Item.tsx
@@ -1,61 +1,149 @@
 import { ProductWithId } from "@/types/models/product.type";
 import styled from "styled-components";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
-const Item = ({ data }: { data: ProductWithId }) => {
+export default function Item({ data }: { data: ProductWithId }) {
   const { name, link, src, major_category, middle_category, price, piece, price_per_piece, nutritional } = data;
   const { calories, carbohydrate, protein, fat } = nutritional;
+
   return (
-    <Container href={link} target="_blank" rel="noopener noreferrer">
-      <div style={{display:"flex",}}>
+    <Card>
+      <ScrollContainer>
+        {/* Left Page */}
+        <Page>
+          <ImageContainer>
+            <StyledImage src={src} alt={name} />
+          </ImageContainer>
+          <Title>{name}</Title>
+          <Category>{major_category}</Category>
+          <PriceContainer>
+            <div>
+              <Price>{price}원</Price>
+              <PricePerUnit>{price_per_piece}원/개</PricePerUnit>
+            </div>
+            <ShoppingCartIconContaienr>
+              <ShoppingCartIcon />
+            </ShoppingCartIconContaienr>
+          </PriceContainer>
+        </Page>
 
-      <Top>
-        <Img src={src} alt="" />
-        <div>상품명 : {name}</div>
-        <div>
-          {major_category} / {middle_category}
-        </div>
-        <div>
-          가격 : {price} 원 / 개수 : {piece}
-        </div>
-        <div>개당 가격 : {price_per_piece}</div>
-      </Top>
-      <Bottom>
-        <div>칼로리 : {calories}</div>
-        <div>탄수화물 : {carbohydrate}</div>
-        <div>단백질 : {protein}</div>
-        <div>지방 : {fat}</div>
-      </Bottom>
-      </div>
-    </Container>
+        {/* Right Page */}
+        <Page>
+          <NutritionTitle>영양 성분</NutritionTitle>
+          <NutritionItem>
+            <span>칼로리</span>
+            <span>{calories} kcal</span>
+          </NutritionItem>
+          <NutritionItem>
+            <span>단백질</span>
+            <span>{protein}g</span>
+          </NutritionItem>
+          <NutritionItem>
+            <span>탄수화물</span>
+            <span>{carbohydrate}g</span>
+          </NutritionItem>
+          <NutritionItem>
+            <span>지방</span>
+            <span>{fat}g</span>
+          </NutritionItem>
+        </Page>
+      </ScrollContainer>
+    </Card>
   );
-};
+}
 
-export default Item;
-
-const Container = styled.a`
-  display: flex;
-  width:200px;
-  height: 400px;
-  overflow-x:scroll;
-  scroll-snap-type: x mandatory;
-  border: 3px solid #26539c;
-  text-decoration: none;
-  color: inherit;
-  cursor: pointer;
-`;
-
-const Img = styled.img`
-  width: 200px; /* 원하는 너비 */
-  height: auto;
-`;
-
-const Top = styled.div`
-  scroll-snap-align: center;
-  background-color: lightblue;
+const Card = styled.div`
   width: 200px;
-  height: 400px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  border-radius: 0.5rem;
 `;
 
-const Bottom = styled(Top)`
-  background-color: yellow;
+const ScrollContainer = styled.div`
+  display: flex;
+  scroll-snap-type: x mandatory;
+  overflow-x: auto;
+  /* 스크롤바 제거 */
+  scrollbar-width: none; /* Firefox */
+  -ms-overflow-style: none; /* IE/Edge */
+  &::-webkit-scrollbar {
+    display: none; /* Chrome, Safari */
+  }
+`;
+
+const Page = styled.div`
+  width: 200px;
+  /* width: 100%; */
+  scroll-snap-align: start;
+  flex-shrink: 0;
+  padding: 20px;
+`;
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: 100%;
+  height: 12rem;
+  margin-bottom: 1rem;
+`;
+
+const StyledImage = styled.img`
+  width: 100%;
+  border-radius: 0.5rem;
+`;
+
+const Title = styled.h2`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 0.25rem;
+`;
+
+const Category = styled.p`
+  font-size: 0.875rem;
+  color: #6b7280;
+  margin-bottom: 1rem;
+`;
+
+const PriceContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Price = styled.p`
+  font-size: 1.125rem;
+  font-weight: 600;
+`;
+
+const PricePerUnit = styled.p`
+  font-size: 0.875rem;
+  color: #6b7280;
+`;
+
+const AddToCartButton = styled.button`
+  background-color: #3b82f6;
+  color: white;
+  padding: 0.5rem 1rem;
+  border-radius: 9999px;
+  font-weight: 500;
+`;
+
+const NutritionTitle = styled.h3`
+  font-size: 1.25rem;
+  font-weight: 600;
+  margin-bottom: 1rem;
+`;
+
+const NutritionItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 0.5rem;
+`;
+
+const ShoppingCartIconContaienr = styled.div`
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100%;
+  border: 1px solid black;
 `;
