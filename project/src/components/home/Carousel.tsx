11e63c403e2bc9cf5eda38c33ff85eb3ec49c6b2
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-12-02 14:19:02 +0900

    ✨ Feat : 캐러셀 자동 슬라이드 기능 추가

--- project/src/components/home/Carousel.tsx
+++ project/src/components/home/Carousel.tsx
@@ -2,6 +2,7 @@
 
 import styled from "styled-components";
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
 import "swiper/css";
 
 const Carousel = () => {
@@ -11,15 +12,20 @@ const Carousel = () => {
       slidesPerView={1} // 한 번에 보여줄 슬라이드 수
       onSlideChange={() => console.log("Slide changed")}
       onSwiper={(swiper) => console.log(swiper)}
+      modules={[Autoplay]}
+      autoplay={{
+        delay: 3000, // 3초마다 전환
+        disableOnInteraction: false, // 사용자 상호작용 후에도 자동 재생 유지
+      }}
     >
       <SwiperSlide>
-        <img src="/vercel.svg"/>
+        <img src="/vercel.svg" />
       </SwiperSlide>
       <SwiperSlide>
-        <img src="/vercel.svg"/>
+        <img src="/vercel.svg" />
       </SwiperSlide>
       <SwiperSlide>
-        <img src="/vercel.svg"/>
+        <img src="/vercel.svg" />
       </SwiperSlide>
     </Container>
   );
