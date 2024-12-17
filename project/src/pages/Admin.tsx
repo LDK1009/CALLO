5253e24c0356508061b54af24817e245bd2a6ded
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-12-17 12:32:55 +0900

    💄 Style : 헤더 스타일 수정으로 인한 어드민 페이지 스타일 수정

--- project/src/pages/Admin.tsx
+++ project/src/pages/Admin.tsx
@@ -2,18 +2,17 @@
 import Dashboard from "@/components/admin/Dashboard";
 import SignIn from "@/components/admin/SignIn";
 import { useState } from "react";
-
- 
-
+import styled from "styled-components";
 
 const Admin = () => {
-    const [isSignIn, setIsSignIn] = useState<boolean>(false);
+  const [isSignIn, setIsSignIn] = useState<boolean>(false);
 
-    return (
-        <>
-        {isSignIn ? <Dashboard/> : <SignIn setIsSignIn={setIsSignIn} />}        
-        </>
-    );
+  return <Conatainer>{isSignIn ? <Dashboard /> : <SignIn setIsSignIn={setIsSignIn} />}</Conatainer>;
 };
 
 export default Admin;
+
+const Conatainer = styled.div`
+
+  padding-top: 50px;
+`;
