bd95536e1fd0b6705e5df3b21d4c2928a4586414
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-12-21 23:50:43 +0900

    🔨 Refactor : 타입 수정

--- project/src/types/store/product.type.ts
+++ project/src/types/store/product.type.ts
@@ -5,14 +5,14 @@ export type ProductState = {
 
 export type Product = {
   id?: number;
-  created_at: Date;
+  created_at?: Date;
   name: string; // 제품 이름 (varchar)
   link: string; // 제품 링크 (varchar)
   src: string; // 소스 URL 또는 파일 경로 (varchar)
-  major_category: string; // 주요 카테고리 (varchar)
-  middle_category: string; // 중간 카테고리 (varchar)
+  major_category?: string; // 주요 카테고리 (varchar)
+  middle_category?: string; // 중간 카테고리 (varchar)
   price: number;
-  piece: number;
+  piece?: number;
   price_per_piece: number;
   nutritional: Nutritional; // 영양 정보 (json)
 };
