9583af4c9e7c5f7cd6ab90a7177177bb90e120c4
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2024-12-24 15:52:11 +0900

    ✨ Feat : 바로가기 버튼 개발

---
+++ project/src/components/my-page/DataLabAndCart.tsx
@@ -0,0 +1,69 @@
+"use client";
+
+import { PentagonOutlined } from "@mui/icons-material";
+import Link from "next/link";
+import styled from "styled-components";
+
+const DataLabAndCart = () => {
+  return (
+    <Container>
+      <GotoButton href="/data-lab">
+        <IconBox>
+          <PentagonOutlined />
+        </IconBox>
+        <Text>데이터랩</Text>
+      </GotoButton>
+      <GotoButton href="/cart">
+        <IconBox>
+          <PentagonOutlined />
+        </IconBox>
+        <Text>장바구니</Text>
+      </GotoButton>
+    </Container>
+  );
+};
+
+export default DataLabAndCart;
+
+const Container = styled.div`
+  width: 375px;
+  height: 200px;
+  padding: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const GotoButton = styled(Link)`
+  width: 160px;
+  height: 160px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid var(--main-color);
+  border-radius: 12px;
+  box-shadow: 4px 4px 16px 0px var(--main-color);
+`;
+
+const IconBox = styled.div`
+  width: 100%;
+  height: 130px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  & > svg {
+    width: 100px;
+    height: 100px;
+    color: var(--main-color);
+  }
+`;
+
+const Text = styled.div`
+  width: 100%;
+  height: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 20px;
+`;
--- project/src/pages/MyPage.tsx
+++ project/src/pages/MyPage.tsx
@@ -1,9 +1,11 @@
+import DataLabAndCart from "@/components/my-page/DataLabAndCart";
 import Profile from "@/components/my-page/Profile";
 
 const MyPage = () => {
     return (
         <div>
             <Profile/>
+            <DataLabAndCart/>
         </div>
     );
 };
