21970217f2d0493a15bab067ed1bd28df03cc37d
Soon_Coding <86905679+LDK1009@users.noreply.github.com>
2025-01-06 10:11:41 +0900

    💄 Style : <Tag> 스타일링

--- project/src/components/common/Tag.tsx
+++ project/src/components/common/Tag.tsx
@@ -10,9 +10,10 @@ const Container = styled.div`
   width: 60px;
   height: 20px;
   margin-right: 12px;
-  background: linear-gradient(to right, var(--serve-color1), white);
+  background: linear-gradient(45deg, var(--serve-color1) 50%, var(--main-color) 100%); 
   border-radius: 10px;
   font-size: 8px;
+  color:white;
   display: flex;
   align-items: center;
   justify-content: center;
